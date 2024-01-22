de21db660c0642d6f3b5109247436c4f05ab938b
Juan Martin <juanmartinsesali@gmail.com>
2024-01-22 11:52:25 -0300

    Initial Commit

---
+++ renamer/renamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  renamerApp.swift
+//  renamer
+//
+//  Created by Juan Martin on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct renamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
